0de274f560c4ede0294444d9464113cb561d63d8
dan-nl <d_entous@yahoo.com>
2013-10-03 21:34:24 +0200

    merging with php branch version
    Change-Id: Ic1b0e5ad80605a9bf4cf8e8c0b90d100aadfdb6b

--- includes/Config.php
+++ includes/Config.php
@@ -6,6 +6,7 @@
  * @ingroup Extensions
  * @license GNU General Public License 3.0 http://www.gnu.org/licenses/gpl.html
  */
+
 namespace GWToolset;
 
 class Config {
@@ -17,15 +18,44 @@ class Config {
 	public static $type = 'media';
 	public static $version = '0.0.1';
 
+	/**
+	 * @var {array}
+	 * which extension/mimetype combinations should the extension accept
+	 * for mapping files
+	 */
+	public static $accepted_mapping_types = array(
+		'json' => array( 'application/json' )
+	);
+
+	/**
+	 * @var {array}
+	 * which extension/mimetype combinations should the extension accept
+	 * for metadata files
+	 */
+	public static $accepted_metadata_types = array(
+		'xml' => array( 'text/xml', 'application/xml' )
+	);
+
+	/**
+	 * @var {array}
+	 * which MediaWiki Templates are allowed for mapping
+	 */
+	public static $allowed_templates = array(
+		'Artwork',
+		'Book',
+		'Musical_work',
+		'Photograph',
+		'Specimen'
+	);
+
+	/**
+	 * @var {array}
+	 */
 	public static $autoloader_classes = array(
 		'GWToolset\Adapters\DataAdapterInterface' => '/includes/Adapters/DataAdapterInterface.php',
 
-		'GWToolset\Adapters\Db\DbAdapterAbstract' => '/includes/Adapters/Db/DbAdapterAbstract.php',
-		'GWToolset\Adapters\Db\MediawikiTemplateDbAdapter' => '/includes/Adapters/Db/MediawikiTemplateDbAdapter.php',
-
 		'GWToolset\Adapters\Php\MappingPhpAdapter' => '/includes/Adapters/Php/MappingPhpAdapter.php',
-
-		'GWToolset\Exception' => '/includes/Exception.php',
+		'GWToolset\Adapters\Php\MediawikiTemplatePhpAdapter' => '/includes/Adapters/Php/MediawikiTemplatePhpAdapter.php',
 
 		'GWToolset\Forms\MetadataDetectForm' => '/includes/Forms/MetadataDetectForm.php',
 		'GWToolset\Forms\MetadataMappingForm' => '/includes/Forms/MetadataMappingForm.php',
@@ -45,6 +75,7 @@ class Config {
 		'GWToolset\Handlers\Xml\XmlMappingHandler' => '/includes/Handlers/Xml/XmlMappingHandler.php',
 
 		'GWToolset\Helpers\FileChecks' => '/includes/Helpers/FileChecks.php',
+		'GWToolset\Helpers\FileSystem' => '/includes/Helpers/FileSystem.php',
 		'GWToolset\Helpers\WikiChecks' => '/includes/Helpers/WikiChecks.php',
 		'GWToolset\Helpers\WikiPages' => '/includes/Helpers/WikiPages.php',
 
@@ -68,18 +99,133 @@ class Config {
 		'Php\FilterException' => '/includes/Php/FilterException.php'
 	);
 
+	/**
+	 * @var {string}
+	 */
+	public static $category_separator = '|';
+
+	/**
+	 * @var {array}
+	 */
+	public static $hooks = array(
+		'CanonicalNamespaces' => 'GWToolset\Hooks::onCanonicalNamespaces'
+	);
+
+	/**
+	 * @var {int}
+	 * 20 minutes, 25 seconds default
+	 */
+	public static $http_timeout = 1200;
+
+	/**
+	 * @var {string}
+	 */
+	public static $http_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1';
+
+	/**
+	 * @var {array}
+	 */
+	public static $jobs = array(
+		'gwtoolsetUploadFromUrlJob' => 'GWToolset\Jobs\UploadFromUrlJob',
+		'gwtoolsetUploadMediafileJob' => 'GWToolset\Jobs\UploadMediafileJob',
+		'gwtoolsetUploadMetadataJob' => 'GWToolset\Jobs\UploadMetadataJob'
+	);
+
+	/**
+	 * @var {int}
+	 */
+	public static $job_throttle = 3;
+
+	/**
+	 * @var {int}
+	 * 1.25e7 or 12,500,000 default
+	 */
+	public static $max_image_area = 6.4e7;
+
+	/**
+	 * @var {int}
+	 * set in bytes
+	 * the maximum upload filesize this extension will accept. when set to 0,
+	 * the wiki’s $wgMaxUploadSize is used
+	 */
+	public static $max_upload_filesize = 0;
+
+	/**
+	 * @var {array}
+	 * http://gwtoolset/api.php?action=templatedata&titles=Template:Artwork
+	 */
+	public static $mediawiki_templates = array(
+		'Artwork' => '{"artist":"","title":"","description":"","date":"","medium":"","dimensions":"","institution":"","location":"","references":"","object history":"","exhibition history":"","credit line":"","inscriptions":"","notes":"","accession number":"","source":"","permission":"","other_versions":""}',
+		'Book' => '{"Author":"","Translator":"","Editor":"","Illustrator":"","Title":"","Subtitle":"","Series title":"","Volume":"","Edition":"","Publisher":"","Printer":"","Date":"","City":"","Language":"","Description":"","Source":"","Permission":"","Image":"","Image page":"","Pageoverview":"","Wikisource":"","Homecat":"","Other_versions":"","ISBN":"","LCCN":"","OCLC":""}',
+		'Musical work' => '{"composer":"","lyrics_writer":"","performer":"","title":"","description":"","composition_date":"","performance_date":"","notes":"","record_ID":"","image":"","references":"","source":"","permission":"","other_versions":""}',
+		'Photograph' => '{"photographer":"","title":"","description":"","depicted people":"","depicted place":"","date":"","medium":"","dimensions":"","institution":"","department":"","references":"","object history":"","exhibition history":"","credit line":"","inscriptions":"","notes":"","accession number":"","source":"","permission":"","other_versions":""}',
+		'Specimen' => '{"taxon":"","authority":"","institution":"","accession number":"","sex":"","discovery place":"","cultivar":"","author":"","source":"","date":"","description":"","period":"","depicted place":"","camera coord":"","dimensions":"","institution":"","location":"","object history":"","exhibition history":"","credit line":"","notes":"","references":"","permission":"","other versions":"","photographer":"","source":""}'
+	);
+
+	/**
+	 * @var {string}
+	 * wiki namespace to store metadata mappings and data sets
+	 */
+	public static $mediafile_namespace = NS_FILE;
+
+	/**
+	 * @var {string}
+	 * 128M default
+	 */
+	public static $memory_limit = '256M';
+
+	/**
+	 * @var {array}
+	 */
 	public static $messages = array(
+		'GWToolsetAlias' => '/GWToolset.alias.php',
 		'GWToolset' => '/GWToolset.i18n.php',
-		'GWToolsetAlias' => '/GWToolset.alias.php'
+		'GWToolsetNamespaces' => '/GWToolset.namespaces.php',
 	);
 
-	public static $special_pages = array(
-		'GWToolset' => array(
-			'class_name' => 'GWToolset\SpecialGWToolset',
-			'group' => 'media'
-		)
-	);
+	/**
+	 * @var {string}
+	 * category automatically assigned to metadata files uploaded by GWToolset
+	 */
+	public static $metadata_file_category = 'GWToolset_Metadata_Sets';
 
+	/**
+	 * @var {string}
+	 * wiki namespace to store metadata mappings and data sets
+	 */
+	public static $metadata_namespace = NS_GWTOOLSET;
+
+	/**
+	 * @var {string}
+	 * subpage used to place saved metadata mappings
+	 */
+	public static $metadata_mapping_subpage = 'Metadata_Mappings';
+
+	/**
+	 * @var {string}
+	 */
+	public static $metadata_separator = '; ';
+
+	/**
+	 * @var {string}
+	 * subpage used to place saved metadata sets
+	 */
+	public static $metadata_sets_subpage = 'Metadata_Sets';
+
+	/**
+	 * @var {string}
+	 * category automatically added to items uploaded by GWToolset
+	 */
+	public static $mediawiki_template_default_category = 'GWToolset_Batch_Upload';
+
+	/**
+	 * @var {int}
+	 */
+	public static $preview_throttle = 3;
+
+	/**
+	 * @var {array}
+	 */
 	public static $resources = array(
 		'scripts' => array(
 			'resources/js/ext.gwtoolset.js'
@@ -111,140 +257,76 @@ class Config {
 		)
 	);
 
-	public static $hooks = array(
-		'LoadExtensionSchemaUpdates' => 'GWToolset\Hooks::onLoadExtensionSchemaUpdates'
-	);
-
-	public static $jobs = array(
-		'gwtoolsetUploadFromUrlJob' => 'GWToolset\Jobs\UploadFromUrlJob',
-		'gwtoolsetUploadMediafileJob' => 'GWToolset\Jobs\UploadMediafileJob',
-		'gwtoolsetUploadMetadataJob' => 'GWToolset\Jobs\UploadMetadataJob'
-	);
+	/**
+	 * @var {string}
+	 * Category:Source_templates is the category on commons for partner templates
+	 */
+	public static $source_templates = 'Source_templates';
 
 	/**
 	 * @see SpecialPage __constructor
-	 *
-	 * $name string
+	 * @var {string}
 	 * name of the special page, as seen in links and URLs
-	 *
-	 * $restriction string
-	 * user right required, e.g. "block" or "delete"
-	 *
-	 * $listed boolean
-	 * whether the page is listed in Special:Specialpages
 	 */
 	public static $special_page_name = 'GWToolset';
-	public static $restriction = 'upload_by_url';
-	public static $listed = true;
 
 	/**
-	 * @var {int}
-	 * set in bytes
-	 * the maximum upload filesize this extension will accept. when set to 0,
-	 * the wiki’s $wgMaxUploadSize is used
+	 * @see SpecialPage __constructor
+	 * @var {string}
+	 * user right required, e.g. "block" or "delete"
 	 */
-	public static $max_upload_filesize = 0;
+	public static $special_page_restriction = 'upload_by_url';
 
 	/**
-	 * the user group the user must be a member of in order to be able to use this extension
-	 * @see GWToolset\Helpers\WikiChecks\checkUserWikiGroups
+	 * @see SpecialPage __constructor
+	 * @var {boolean}
+	 * whether the page is listed in Special:Specialpages
 	 */
-	public static $user_group = 'gwtoolset';
+	public static $special_page_listed = true;
 
 	/**
-	 * user permissions required in order to be able to use this extension
-	 * @see GWToolset\Helpers\WikiChecks\checkUserPermissions
+	 * @var {array}
 	 */
-	public static $user_permissions = array( 'upload', 'upload_by_url', 'edit' );
+	public static $special_pages = array(
+		'GWToolset' => array(
+			'class_name' => 'GWToolset\SpecialGWToolset',
+			'group' => 'media'
+		)
+	);
 
 	/**
+	 * @var {string}
+	 */
+	public static $title_separator = '-';
+
+	/**
+	 * @var {boolean}
 	 * tells the upload form to place the $accepted_mime_types in a comma
 	 * delimited list in the input file’s accept attribute
 	 */
 	public static $use_file_accept_attribute = true;
 
 	/**
-	 * which extension/mimetype combinations should the extension accept
-	 * for mapping files
+	 * @var {boolean}
 	 */
-	public static $accepted_mapping_types = array(
-		'json' => array( 'application/json' )
-	);
+	public static $use_UploadStash = true;
 
 	/**
-	 * which extension/mimetype combinations should the extension accept
-	 * for media files
+	 * @var {string}
+	 * the user group the user must be a member of in order to be able to use this extension
+	 * @see GWToolset\Helpers\WikiChecks\checkUserWikiGroups
 	 */
-	public static $accepted_media_types = array(
-		'jpg' => array( 'image/jpeg' ),
-		'pdf' => array( 'application/pdf' )
-	);
+	public static $user_group = 'gwtoolset';
 
 	/**
-	 * which extension/mimetype combinations should the extension accept
-	 * for metadata files
+	 * @var {array}
+	 * user permissions required in order to be able to use this extension
+	 * @see GWToolset\Helpers\WikiChecks\checkUserPermissions
 	 */
-	public static $accepted_metadata_types = array(
-		'xml' => array( 'text/xml', 'application/xml' )
+	public static $user_permissions = array(
+		'upload',
+		'upload_by_url',
+		'edit'
 	);
 
-	/**
-	 * which MediaWiki Templates are allowed for mapping
-	 */
-	public static $allowed_templates = array(
-		'Artwork',
-		'Book',
-		'Musical work',
-		'Photograph',
-		'Specimen'
-	);
-
-	public static $category_separator = '|';
-	public static $metadata_separator = '; ';
-	public static $title_separator = '-';
-
-	/**
-	 * note that this mapping tag has a hardcoded pregmatch in
-	 * GWToolset/includes/Adapters/Api/MappingApiAdapter.php
-	 * that needs to change if this value changes
-	 */
-	public static $metadata_mapping_open_tag = '<mapping_json>';
-	public static $metadata_mapping_close_tag = '</mapping_json>';
-
-	// wiki namespace to store metadata mappings and data sets
-	public static $metadata_namespace = 'GWToolset:';
-
-	// wiki namespace to store metadata mappings and data sets
-	public static $mediafile_namespace = 'File:';
-
-	// sub directory used to place saved metadata mappings
-	public static $metadata_mapping_subdirectory = 'Metadata Mappings';
-
-	// sub directory used to place saved metadata sets
-	public static $metadata_sets_subdirectory = 'Metadata Sets';
-
-	// category automatically assigned to saved metadata mappings
-	public static $metadata_mapping_category = 'GWToolset Metadata Mappings';
-
-	// category automatically added to items uploaded by GWToolset
-	public static $mediawiki_template_default_category = 'GWToolset Batch Upload';
-
-	// category automatically assigned to metadata files uploaded by GWToolset
-	public static $metadata_file_category = 'GWToolset Metadata Sets';
-
-	// Category:Source_templates is the category on commons for partner templates
-	public static $source_templates = 'Source templates';
-
-	public static $job_throttle = 3;
-
-	public static $preview_throttle = 3;
-
-	// 20 minutes, 25 seconds default
-	public static $http_timeout = 1200;
-
-	// 12500000 default
-	public static $max_image_area = 64000000;
-
-	// 128M default
-	public static $memory_limit = '256M';
 }
